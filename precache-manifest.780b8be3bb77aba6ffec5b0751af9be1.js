128d4e872e1596bef9146fb45493b9fc7f40af36
danigb <danigb@gmail.com>
2020-05-27 10:58:29 +0200

    Updates

---
+++ precache-manifest.780b8be3bb77aba6ffec5b0751af9be1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e08bc62795123935da72244d751d120",
+    "url": "/react-mb/index.html"
+  },
+  {
+    "revision": "be40d91f27d846567509",
+    "url": "/react-mb/static/css/main.59e5f56b.chunk.css"
+  },
+  {
+    "revision": "27f09e5ed219f423ea60",
+    "url": "/react-mb/static/js/2.55ca4ba7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mb/static/js/2.55ca4ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be40d91f27d846567509",
+    "url": "/react-mb/static/js/main.79a05852.chunk.js"
+  },
+  {
+    "revision": "5981e7b66d8e017d347b",
+    "url": "/react-mb/static/js/runtime-main.bafca4d4.js"
+  }
+]);
